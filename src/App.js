48ad747c5feca476bbd5d5ba1ec461e0ed769ad9
Dmitri <LarionovDmitri@gmail.com>
2019-12-27 15:03:44 -0600

    added links to gcloud hosted react auth demo app for secure and unsecure

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import "./App.css";
 
 function App() {
   const localRedirectProjectUrl = "http://localhost:8080";
+  const authDemoAppGcloudHosted = "https://test-production-build-with-ssr.appspot.com"
+  const authDemoAppGcloudHostedNotSecure = "http://test-production-build-with-ssr.appspot.com"
   const authDemoAppUrl = "https://boring-lewin-b8cd3f.netlify.com/";
-  const gcloudUrl = "https://devenv-259801.appspot.com"
+  const gcloudUrl = "https://devenv-259801.appspot.com/reg"
+  const gcloudUrlSec = "https://devenv-259801.appspot.com/gcloudSec"
+  const gcloudUrlUnsec = "https://devenv-259801.appspot.com/gcloudUnsec"
   const namedTarget = "named_frame";
   const form1_handlder = form => {
     let formToSubmit = document.getElementById(form);
@@ -46,12 +50,42 @@ function App() {
         </div>
       </div>
       <div>FORM BASED REDIRECT</div>
-      <div>To GCloud DEV ENV</div>
+      <div>To GCloud DEV ENV redirect to Netlify Hosted App</div>
       <div>
         <form
           id="form_1"
           method="post"
           action={gcloudUrl}
+          target={namedTarget}
+        >
+          <input type="text" name="id" defaultValue="234234234"></input>
+          <input type="text" name="idType" defaultValue="secretId"></input>
+          <a href="javascript:{}" onClick={() => form1_handlder("form_1")}>
+            Named Target Submit Form
+          </a>
+        </form>
+      </div>
+      <div>To GCloud DEV ENV redirect to Gcloud Secure</div>
+      <div>
+        <form
+          id="form_1"
+          method="post"
+          action={gcloudUrlSecure}
+          target={namedTarget}
+        >
+          <input type="text" name="id" defaultValue="234234234"></input>
+          <input type="text" name="idType" defaultValue="secretId"></input>
+          <a href="javascript:{}" onClick={() => form1_handlder("form_1")}>
+            Named Target Submit Form
+          </a>
+        </form>
+      </div>
+      <div>To GCloud DEV ENV Redirect to gcloud Unsecure</div>
+      <div>
+        <form
+          id="form_1"
+          method="post"
+          action={gcloudUrlUnsec}
           target={namedTarget}
         >
           <input type="text" name="id" defaultValue="234234234"></input>
